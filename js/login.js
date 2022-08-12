ce59ec52f8399dcde48b3f348ac66b9b8e22f014
W Rivas <rivas.will.1000@gmail.com>
2022-08-12 17:14:18 -0300

    update

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const mail = document.getElementById('email');
+const psswd = document.getElementById('psswd');
+const mail_label = document.getElementById('email_label');
+const psswd_label = document.getElementById('psswd_label');
+
+
+// mail.addEventListener('click', ()=>{
+//     mail_label.className.add('cntn-label')
+//   })
+// forms-label
