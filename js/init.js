8820b4f6c1330f9ae3e83af0a1f87d38659fdc53
rivas.will <rivas.will.1000@gmail.com>
2022-10-01 19:33:46 -0300

    entrega 4

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
-const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
-const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
-const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
-const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
-const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json"; //categorias
+const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json"; //"msg": "¡Has publicado con éxito!"
+const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/"; //productos por categoria -> idCat.json
+const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/"; //info del producto -> idProd.json
+const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/"; //comentarios del producto -> idProd.json
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
-const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json"; //"msg": "¡Has comprado con éxito!"
 const EXT_TYPE = ".json";
 
 let showSpinner = function(){
@@ -42,13 +42,37 @@ let getJSONData = function(url){
 
 
 //localStorage
-document.addEventListener('DOMContentLoaded', ()=>{
 
+document.addEventListener('DOMContentLoaded', ()=>{
+  
   //user
   let username = document.querySelector('.nav-item:last-child');
+  if(localStorage.mail==undefined||localStorage.mail==''){
+    localStorage.setItem('mail','Iniciar Sesión');
+  }
   if(username != null) {
-    username.innerHTML = `<a class="nav-link" href="">${localStorage.mail}</a>`
-  } 
+    if(localStorage.mail=='Iniciar Sesión') {
+      username.innerHTML = `
+      <a class="nav-link" href="./index.html">
+        ${localStorage.mail}
+      </a>`
+    }else{
+      username.innerHTML = `
+      <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+        ${localStorage.mail}
+      </a>
+      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark" aria-labelledby="dropdownMenuLink">
+        <li><a class="dropdown-item" href="./product-info.html">Mi carrito</a></li>
+        <li><a class="dropdown-item" href="./my-profile.html">Mi perfil</a></li>
+        <li><a class="dropdown-item" href="./index.html"
+                onclick='()=>{localStorage.removeItem('mail')}'>Cerrar sesión</a></li>
+      </ul>
+      `
+      document.querySelector('.nav-item:first-child>a').setAttribute('href','./login.html');
+  
+    }
+    username.className+=' dropdown'
+  }
   
 });
 
